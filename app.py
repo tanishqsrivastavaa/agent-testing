73a962135fe61a2e167fbef1a905fc6cf8252ab6
tanishqsrivastavaa <tanishq280205@gmail.com>
2025-03-04 10:27:44 +0530

    -

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-from smolagents import SmolAgent, HfApiModel
+from pydantic_ai import Agent, RunContext
 
 
-class DuckDuckGoSearchTool(Tool):
-    def run(self,query : str) -> str:
-        return f'Results for : {query}'
-    
-
-agent = SmolAgent(tools=[DuckDuckGoSearchTool()])
-
-prompt = input("Enter a prompt:")
-response = agent.run(prompt)
-
-print(response)
+print("test")
